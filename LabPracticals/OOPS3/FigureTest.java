e50892e933b90a5742dc618e5f13ad8444c7d04f
ChoxV <165756031+ChoxV@users.noreply.github.com>
2024-11-23 02:08:35 +0530

    Add files via upload

---
+++ LabPracticals/OOPS3/FigureTest.java
@@ -0,0 +1,44 @@
+public class FigureTest {
+    public static void main(String[] args){
+        Cone cone = new Cone(5, 10, 7);
+        cone.calcArea();
+        cone.calcVolume();
+        cone.dispArea();
+        cone.dispVolume();
+    }
+    
+}
+class Figure{
+    protected double r;
+    protected double a;
+    protected double v;
+   
+    Figure(double r, double a, double v){
+        this.r = r;
+        this.a = a;
+        this.v = v;
+    }
+    public void dispArea(){
+        System.out.println("Area: "+a);
+    }
+    public void dispVolume(){
+        System.out.println("Volume is :"+v);
+    }
+}
+class Cone extends Figure{
+    private double h;
+    private double  s;
+    public Cone( double r, double h, double s){
+        super(r,0,0);
+        this.h = h;
+        this.s = s;
+    }
+    public void calcArea(){
+        a = Math.PI*(r*s+ r*r);
+        System.out.println("The calculated area of cone is "+a);
+    }
+    public void calcVolume(){
+        v = Math.PI*h*r*r/3;
+        System.out.println("The calculated volume of cone is "+v);
+    }
+}
